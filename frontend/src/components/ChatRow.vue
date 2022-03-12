64886f2ffdc8c29512ea0c0633e2a2b28b74407f
twinkletwinklelittlestar70 <linqianqian70@outlook.com>
2022-03-12 19:45:45 +0800

    feat: basic UI

---
+++ frontend/src/components/ChatRow.vue
@@ -0,0 +1,84 @@
+<template>
+  <div :class="['row-container', { 'style-right': isRight }]"> 
+      <div class="flex-left">
+          <div class="avator"></div>
+          <div class="username">{{ username }}</div>
+      </div>
+      <div class="flex-right">
+        <div class="content-bg">
+            <div class="content">{{ content }}</div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ChatRow',
+  props: {
+    isRight: {
+      type: Boolean,
+      default: false
+    },
+    username: String,
+    content: String,
+  },
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.row-container {
+    display: flex;
+    justify-content: space-between;
+}
+.style-right {
+    flex-direction: row-reverse;
+}
+.avator {
+    width: 30px;
+    height: 30px;
+    background: bisque;
+    border-radius: 50%;
+    margin-top: 10px;
+}
+.style-right .avator, .style-right .content-bg {
+    background: #abdbda;
+}
+.username {
+    font-size: 12px;
+}
+.flex-left {
+    width: 15%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.flex-right {
+    flex: 1;
+    position: relative;
+}
+.content-bg {
+    margin: 10px 20px;
+    line-height: 30px;
+    background: bisque;
+    padding: 5px 20px;
+    left: 0;
+    border-radius: 5px;
+}
+.content {
+    text-align: left;
+}
+</style>
---
+++ frontend/src/components/UserInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="input-container">
+      <div class="send-options">
+          <span class="option-label">Q & A</span>
+          <span class="option-label">Fake detector</span>
+          <span class="option-label">Cough</span>
+      </div>
+      <div class="send-box">
+            <el-input v-model="input" placeholder="Please input" v-on:change="sendMessage" />
+            <el-button>send</el-button>
+      </div>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'UserInput',
+  props: {
+
+  },
+  data() {
+    return {
+        input: ''
+    };
+  },
+  components: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input-container {
+    position: relative;
+    background: #abdbda;
+    padding: 20px;
+}
+.option-label {
+    background: white;
+    text-align: left;
+    font-size: 12px;
+    padding: 5px;
+    margin: 2px;
+    border-radius: 3px;
+}
+.send-box {
+    display: flex;
+}
+.send-options {
+    text-align: left;
+}
+</style>
